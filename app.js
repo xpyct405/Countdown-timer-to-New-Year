8d27d585383629843bea50dc387906ca9dab1469
Rustam Kuzyaev <xpyct405@yandex.ru>
2023-07-10 21:49:38 +0300

    feat: add popular holidays btns with adaptive date

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+let header = document.querySelector('.header');
 let daysEl = document.getElementById('days');
 let hoursEl = document.getElementById('hours');
 let minsEl = document.getElementById('mins');
 let secondsEl = document.getElementById('seconds');
 
 // тут можно указать любую дату!
-const newYear = '1 Jan 2024';
+let date = '1 Jan 2024';
 
 function countdown() {
-    let newYearDate = new Date(date.value);
+    let newYearDate = new Date(date);
     let currentDate = new Date()
-    console.log(newYearDate, currentDate);
+    // console.log(newYearDate, currentDate);
 
     // Ниже значение в миллиисекундах, делим на 1000. Сттолько всего секунд до нг!
     let totalSecods = Math.floor((newYearDate - currentDate) / 1000)
@@ -23,7 +24,7 @@ function countdown() {
     // Оставляем  остаток после деления на 60 секунд
     let seconds = Math.floor(totalSecods) % 60
 
-    console.log(days, hours, mins, seconds);
+    // console.log(days, hours, mins, seconds);
 
     daysEl.innerHTML = formatTime(days);
     hoursEl.innerHTML = formatTime(hours);
@@ -39,16 +40,52 @@ function formatTime(time) {
 setInterval(countdown, 1000)
 
 
-function getDate() {
-    let date = document.querySelector('#date')
 
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('div')
-    document.querySelector('.input-container').appendChild(newDiv)
-    newDiv.innerHTML = date.value;
+// Popular Holidays
 
-    return;
-    console.log(date.value);
-}
-// getDate()
-console.log(date.value);
+let btnNy = document.querySelector('.btn-ny');
+let btnGd = document.querySelector('.btn-gd');
+let btnSp = document.querySelector('.btn-sp');
+let btnId = document.querySelector('.btn-id');
+let btnHw = document.querySelector('.btn-hw');
+let btnCs = document.querySelector('.btn-cs');
+
+btnNy.addEventListener('click', () => {
+    header.innerHTML = 'New Year 2024';
+    date = '1 Jan 2024';
+})
+btnGd.addEventListener('click', () => {
+    header.innerHTML = 'Groundhog Day 2024';
+    date = '2 Feb 2024';
+})
+btnSp.addEventListener('click', () => {
+    header.innerHTML = "Saint Patrick's Day 2024";
+    date = '18 Mar 2024';
+})
+btnId.addEventListener('click', () => {
+    header.innerHTML = "Independence Day 2024";
+    date = '4 July 2024';
+})
+btnHw.addEventListener('click', () => {
+    header.innerHTML = "Halloween 2024";
+    date = '31 Oct 2024';
+})
+btnCs.addEventListener('click', () => {
+    header.innerHTML = "Christmas 2024";
+    date = '25 Dec 2024';
+})
+
+
+
+
+// function getDate() {
+//     let date = document.querySelector('#date')
+
+//     let newDiv = document.createElement('div');
+//     newDiv.classList.add('div')
+//     document.querySelector('.input-container').appendChild(newDiv)
+//     newDiv.innerHTML = date.value;
+
+//     return;
+// }
+// console.log(date.value);
